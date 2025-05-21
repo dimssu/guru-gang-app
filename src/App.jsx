068f770e483e0dc5fbd8e3d163381772b6a041a3
fl-aryan <aryan@recur.club>
2025-05-21 20:53:28 +0530

    Fixed type

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import AdminHome from "./pages/AdminHome";
+import AdminHome from "./pages/Adminhome";
 import TeacherHome from "./pages/Teacherhome";
 import StudentHome from "./pages/studenthome";
 import Dashboard from "./pages/Dashboard";
